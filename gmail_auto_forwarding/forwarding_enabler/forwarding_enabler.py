cb9dde07480a200bb98f3c00490f221998312d78
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-06-20 00:24:25 +0200

    refactor(forwarding_enabler): Add logging

--- gmail_auto_forwarding/forwarding_enabler/forwarding_enabler.py
+++ gmail_auto_forwarding/forwarding_enabler/forwarding_enabler.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from gmail_auto_forwarding.browser.extra_actions import wait_for_element_until_clickable
+from gmail_auto_forwarding.utils.logger import setup_logger
 
 from .constants import (
     EMAIL_INPUT_ID,
@@ -24,6 +25,8 @@ from .constants import (
     WAIT_TIME_MIN,
 )
 
+logger = setup_logger(logger_name=__name__)
+
 
 def enable_forwarding(
     browser: WebDriver,
@@ -32,11 +35,13 @@ def enable_forwarding(
     forward_filters: Dict[str, Union[List[str], Dict[str, Union[int, bool]]]],
 ) -> None:
     # Go to Gmail
+    logger.info("Going to Gmail...")
     browser.get(GMAIL_URL)
 
     initial_window = browser.current_window_handle
 
     # Login to the forwarder's account
+    logger.info("Logging in to the forwarder's account...")
     wait_for_element_until_clickable(browser, (By.ID, EMAIL_INPUT_ID)).send_keys(forwarder["email"])  # type: ignore
     browser.find_element(By.ID, EMAIL_NEXT_BUTTON_ID).click()
     wait_for_element_until_clickable(browser, (By.NAME, PASSWORD_INPUT_NAME)).send_keys(forwarder["password"])  # type: ignore # noqa: E501
@@ -44,9 +49,13 @@ def enable_forwarding(
     sleep(random.randint(WAIT_TIME_MIN, WAIT_TIME_MAX))
 
     # Go to the forwarding settings
+    logger.info("Going to the forwarding settings...")
     browser.get(FORWARDING_SETTINGS_URL)
 
+    # TODO: Take into account sign in errors
+
     # Ask to enable forwarding
+    logger.info("Asking to enable forwarding...")
     wait_for_element_until_clickable(browser, (By.XPATH, FORWARDING_EMAIL_INPUT_XPATH)).send_keys(receiver["email"])  # type: ignore # noqa: E501
     wait_for_element_until_clickable(browser, (By.CSS_SELECTOR, SUBMIT_BUTTON_CSS_SELECTOR)).click()
     browser.find_element(By.NAME, FORWARDING_NEXT_BUTTON_NAME).click()
