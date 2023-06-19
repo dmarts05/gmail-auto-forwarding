5ba7211df766c944fd0ea1546ed76b18a56584a0
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-06-19 23:33:04 +0200

    feat(forwarding_enabler): Reach code verification step

--- gmail_auto_forwarding/forwarding_enabler/forwarding_enabler.py
+++ gmail_auto_forwarding/forwarding_enabler/forwarding_enabler.py
@@ -21,6 +21,8 @@ def enable_forwarding(
 ) -> None:
     browser.get("https://www.gmail.com")
 
+    initial_window = browser.current_window_handle
+
     wait_for_element_until_clickable(browser, (By.ID, "identifierId")).send_keys(forwarder["email"])  # type: ignore
 
     browser.find_element(By.ID, "identifierNext").click()
@@ -29,14 +31,29 @@ def enable_forwarding(
 
     browser.find_element(By.ID, "passwordNext").click()
 
-    time.sleep(1)
+    time.sleep(random.randint(2, 5))
 
     browser.get("https://mail.google.com/mail/u/0/#settings/fwdandpop")
+    wait_for_element_until_visible(browser, (By.TAG_NAME, "body"))
+    time.sleep(random.randint(2, 5))
 
     wait_for_element_until_clickable(browser, (By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div/div/div/div/div[6]/div/table/tbody/tr[1]/td[2]/div/div[2]/input")).click()  # type: ignore
 
-    wait_for_element_until_clickable(browser, (By.ID, ":6r")).send_keys(receiver["email"])  # type: ignore
+    wait_for_element_until_clickable(browser, (By.CSS_SELECTOR, "div.PN > input")).send_keys(receiver["email"])  # type: ignore
 
     browser.find_element(By.NAME, "next").click()
 
-    time.sleep(10)
+    # Switch to the new window
+    for window_handle in browser.window_handles:
+        if window_handle != initial_window:
+            browser.switch_to.window(window_handle)  # type: ignore
+            break
+
+    wait_for_element_until_clickable(browser, (By.CSS_SELECTOR, "input[type='submit']")).click()  # type: ignore
+
+    # Switch back to the initial window
+    browser.switch_to.window(initial_window)  # type: ignore
+
+    wait_for_element_until_clickable(browser, (By.NAME, "ok")).click()
+
+    time.sleep(30)
